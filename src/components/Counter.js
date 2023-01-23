4d890f68c004681a557a77bb7045d45cf7901352
MargretPetrova <margret.ruseva@gmail.com>
2023-01-24 00:28:15 +0200

    working with multiple slices

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,11 +1,11 @@
 import classes from './Counter.module.css';
 import { useSelector, useDispatch } from 'react-redux';
-import { counterActions } from '../store';
+import { counterActions } from '../store/counterSlice';
 
 const Counter = () => {
 
-  const counter = useSelector(state => state.counter);
-  const show = useSelector(state => state.showCounter);
+  const counter = useSelector(state => state.counter.counter);
+  const show = useSelector(state => state.counter.showCounter);
 
   const dispatch = useDispatch();
 
